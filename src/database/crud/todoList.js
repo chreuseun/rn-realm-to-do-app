e02fe1504a95f8bad49db98f8a9d178b5129d2e7
chreuseun <eunillevillegaseunille@gmail.com>
2020-08-22 18:33:09 +0800

    TodoList CRUD operation , deleteAll, selectAll, deleteTodoList

--- src/database/crud/todoList.js
+++ src/database/crud/todoList.js
@@ -5,11 +5,13 @@ import Realm from 'realm';
 import {databaseOptions, schemaNames, schemas} from '../schemas';
 
 const location = 'src/database/crud/todoList.js';
-const { TODO_SCHEMA, TODOLIST_SCHEMA } = schemaNames;
+
+const {defaultDatabaseOption} = databaseOptions;
+const {  TODOLIST_SCHEMA } = schemaNames;
 const { todoSchema, todoListSchema } = schemas;
 
 const  dbOptions  = {
-  ...databaseOptions,
+  ...defaultDatabaseOption,
 	schema: [todoSchema, todoListSchema],
 };
 
@@ -39,11 +41,16 @@ export const updateTodoList = async todoList => {
 		const realm = await Realm.open(dbOptions);
 
 		realm.write(()=>{
-			realm.objectForPrimaryKey(TODOLIST_SCHEMA, todoList.id);
+
+			const {id, name} = todoList;
+
+			const updatingTodoList = realm.objectForPrimaryKey(TODOLIST_SCHEMA, id);
+			updatingTodoList.name = name;
+			return true;
 		});
 	} catch (err) {
 		if (__DEV__){
-			console.log(`ERROR: ${location} > insertNewTodoList: `, err );
+			console.log(`ERROR: ${location} > updateTodoList: `, err );
 		}
 
 		return null;
@@ -51,4 +58,65 @@ export const updateTodoList = async todoList => {
 };
 
 // DELETE todoList
-// EDIT todoList
+export const deleteTodoList = async todoList => {
+	try {
+		const realm = await Realm.open(dbOptions);
+
+		realm.write(()=>{
+			const { id } = todoList;
+			const updatingTodoList = realm.objectForPrimaryKey(TODOLIST_SCHEMA, id);
+			realm.delete(updatingTodoList);
+			return true;
+		});
+	} catch (err) {
+		if (__DEV__){
+			console.log(`ERROR: ${location} > deleteTodoList: `, err );
+		}
+		return null;
+	}
+};
+
+// DELETE ALL todoList
+export const deleteAllTodoList = async () => {
+	try {
+		const realm = await Realm.open(dbOptions);
+
+		realm.write(()=>{
+			realm.deleteAll();
+			return true;
+		});
+	} catch (err) {
+		if (__DEV__){
+			console.log(`ERROR: ${location} > deleteAllTodoList: `, err );
+		}
+		return null;
+	}
+};
+
+// SELECT ALL todoList
+export const selectAllTodoList = async () => {
+	try {
+		const realm = await Realm.open(dbOptions);
+		const allTodoList = realm.objects(TODOLIST_SCHEMA);
+		return allTodoList || [];
+
+	} catch (err) {
+		if (__DEV__){
+			console.log(`ERROR: ${location} > selectAllTodoList: `, err );
+		}
+		return null;
+	}
+};
+
+export default async () => {
+	try {
+		const realm = await Realm.open(dbOptions);
+
+		return realm;
+	} catch (err) {
+		if (__DEV__){
+			console.log(`ERROR: ${location} > export default: `, err );
+		}
+		return null;
+	}
+};
