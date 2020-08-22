174458c6ca446193afab65a3d2e90548b20d68c2
chreuseun <eunillevillegaseunille@gmail.com>
2020-08-22 18:20:20 +0800

    Added INSERT and  UPDATE todoList

--- src/database/crud/todoList.js
+++ src/database/crud/todoList.js
@@ -1,10 +1,54 @@
 /* eslint-disable prettier/prettier */
 
 import Realm from 'realm';
-import {} from './todo
 
-// INSERT todo list
-export const insertNewTodoList = async () => {
+import {databaseOptions, schemaNames, schemas} from '../schemas';
 
+const location = 'src/database/crud/todoList.js';
+const { TODO_SCHEMA, TODOLIST_SCHEMA } = schemaNames;
+const { todoSchema, todoListSchema } = schemas;
+
+const  dbOptions  = {
+  ...databaseOptions,
+	schema: [todoSchema, todoListSchema],
 };
 
+// INSERT todo list
+export const insertNewTodoList = async newTodoList => {
+    try {
+			const realm = await Realm.open(dbOptions);
+
+			realm.write(()=>{
+				realm.create(TODOLIST_SCHEMA, newTodoList , true);
+			});
+
+			return true;
+
+    } catch (err) {
+			if (__DEV__){
+				console.log(`ERROR: ${location} > insertNewTodoList: `, err );
+			}
+
+			return null;
+    }
+};
+
+// UPDATE todo list
+export const updateTodoList = async todoList => {
+	try {
+		const realm = await Realm.open(dbOptions);
+
+		realm.write(()=>{
+			realm.objectForPrimaryKey(TODOLIST_SCHEMA, todoList.id);
+		});
+	} catch (err) {
+		if (__DEV__){
+			console.log(`ERROR: ${location} > insertNewTodoList: `, err );
+		}
+
+		return null;
+	}
+};
+
+// DELETE todoList
+// EDIT todoList
